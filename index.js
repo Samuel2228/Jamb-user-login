b310f5c3c8f415d15c0e7f14794f9e4e1ca40dbb
Samuel2228 <sammycj22@gmail.com>
2023-06-06 13:50:53 +0100

    update

--- index.js
+++ index.js
@@ -4,29 +4,47 @@ let loginBtn = document.getElementById('loginBtn');
 let aTag = document.getElementById('aTag');
 
 
-loginBtn.addEventListener('click', ()=> {
+loginBtn.addEventListener('click', (e)=> {
+    e.preventDefault()
     let newUser = userName.value;
     let newPasswrd = password.value;
     let userPass = newUser + newPasswrd;
 
+    let errors = []
+
+    if(newUser === '')
+    errors = [...errors, 'Enter user name']
+
+    if(newPasswrd === '')
+    errors = [...errors, 'Enter password']
+
+    console.log(errors)
+
+    if(errors.length > 0) {
+        alert(errors.join(', '))
+    } else {
+        const redirect = '/home/jamb-score/user.html'
+       
+        window.location.href = redirect
+    }
+
     
-    if (userPass ==Number()) {
-        alert('Please fill the input');
-        aTag.removeAttribute('href');
-    }
+    // if (userPass ==Number()) {
+    //     alert('Please fill the input');
+    //     aTag.removeAttribute('href');
+    // }
 
 
-    else if (userPass==='') {
-        // alert('Youre now logged in');
-        aTag.removeAttribute('disabled', 'disabled');
-        // aTag.setAttribute('href');
-    }
+    // else if (userPass==='') {
+    //     // alert('Youre now logged in');
+    //     aTag.removeAttribute('disabled', 'disabled');
+    // }
 
-    else {
-        // prompt('which year did you write your Waec?')
-        alert('Youre now logged in');
+    // else {
+    //     // prompt('which year did you write your Waec?')
+    //     alert('Youre now logged in');
         
-        // aTag.setAttribute('disabled');
-    }
+    //     // aTag.setAttribute('disabled');
+    // }
 
 })
